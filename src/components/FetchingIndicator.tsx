e064904723f8fc09803367a74b729625e117b36b
Vladislav Vavilov <maksimsraud@gmail.com>
2024-08-08 21:31:48 +0300

    feat: fetching indicator is delayed

--- src/components/FetchingIndicator.tsx
+++ src/components/FetchingIndicator.tsx
@@ -1,8 +1,21 @@
-import { FC } from 'react'
+import { cn } from '@/utils'
+import { FC, useEffect, useState } from 'react'
 
 export const FetchingIndicator: FC = () => {
+  const [show, setShow] = useState(false)
+
+  useEffect(() => {
+    const timer = setTimeout(() => setShow(true), 400)
+    return () => clearTimeout(timer)
+  }, [])
+
   return (
-    <div className='fixed top-0 z-40 h-1 w-full overflow-hidden bg-blue-200'>
+    <div
+      className={cn(
+        'fixed top-0 z-40 h-1 w-full overflow-hidden bg-blue-200 transition-all duration-200',
+        !show && 'invisible opacity-0'
+      )}
+    >
       <div className='h-full w-full animate-line'>
         <div className='h-full w-1/4 bg-blue-400' />
       </div>
