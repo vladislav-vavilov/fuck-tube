35f3138be6ce1bde2f72d013d226774c378b8811
Vladislav Vavilov <maksimsraud@gmail.com>
2024-08-11 22:07:46 +0300

    fix: added metadata for search results back

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -1,5 +1,17 @@
 import { SearchResultsItems } from '@/components/SearchResults/SearchResultsItems'
 
+type Params = {
+  searchParams: { [key: string]: string | null }
+}
+
+export async function generateMetadata({ searchParams }: Params) {
+  const query = searchParams.search_query
+
+  return {
+    title: query ? query : 'Results'
+  }
+}
+
 export default async function SearchResults() {
   return <SearchResultsItems />
 }
