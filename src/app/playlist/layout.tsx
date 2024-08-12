c591bd40718b8cbf4d9daa375b595c64a0e6c0f9
Vladislav Vavilov <maksimsraud@gmail.com>
2024-08-12 17:58:46 +0300

    fix: minor playlist loading improvements

---
+++ src/app/playlist/layout.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from 'react'
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <div className='mx-auto flex h-full w-full max-w-7xl gap-4 px-4'>
+      {children}
+    </div>
+  )
+}
--- src/components/Playlist/Playlist.tsx
+++ src/components/Playlist/Playlist.tsx
@@ -3,14 +3,10 @@
 import { getPlaylist } from '@/services/api'
 import { useInfiniteQuery } from '@tanstack/react-query'
 import { useSearchParams } from 'next/navigation'
-import { FC, Fragment } from 'react'
-import Image from 'next/image'
-import { Uploader } from '../Uploader'
-import { VideoCard } from '../Cards/VideoCard'
+import { FC } from 'react'
 import { useReachEnd } from '@/hooks/useReachEnd'
 import { PlaylistError } from './PlaylistError'
 import { PlaylistEmpty } from './PlaylistEmpty'
-import { CardSkeletons } from '../Cards/CardSkeletons'
 import { PlaylistLoading } from './PlaylistLoading'
 import { PlaylistInfo } from './PlaylistInfo'
 import { PlaylistVideos } from './PlaylistVideos'
@@ -19,47 +15,35 @@ export const Playlist: FC = () => {
   const searchParams = useSearchParams()
   const list = searchParams.get('list')
 
-  const {
-    data,
-    isFetching,
-    isError,
-    isFetched,
-    fetchNextPage,
-    isFetchingNextPage
-  } = useInfiniteQuery({
-    queryKey: [list],
-    queryFn: ({ pageParam }: { pageParam: string | null }) => {
-      return getPlaylist(list ?? '', pageParam)
-    },
-    initialPageParam: null,
-    getNextPageParam: (lastPage) => lastPage?.nextpage
-  })
+  const { data, isPending, isError, fetchNextPage, isFetchingNextPage } =
+    useInfiniteQuery({
+      queryKey: [list],
+      queryFn: ({ pageParam }: { pageParam: string | null }) => {
+        return getPlaylist(list ?? '', pageParam)
+      },
+      initialPageParam: null,
+      getNextPageParam: (lastPage) => lastPage?.nextpage
+    })
 
   const firstPage = data?.pages[0]
 
-  const ref = useReachEnd<HTMLDivElement>(() => {
+  useReachEnd(() => {
     if (!isFetchingNextPage) fetchNextPage()
   })
 
   if (isError) return <PlaylistError />
+  if (isPending) return <PlaylistLoading />
+  if (!firstPage) return <PlaylistEmpty />
 
   return (
-    <div ref={ref} className='mx-auto h-full w-full max-w-7xl px-4'>
-      {!firstPage && isFetched && <PlaylistEmpty />}
-      <div className='flex gap-4'>
-        {isFetching && !firstPage && <PlaylistLoading />}
-        {firstPage && (
-          <>
-            <PlaylistInfo
-              title={firstPage.name}
-              thumbnail={firstPage.thumbnailUrl}
-              uploaderName={firstPage.name}
-              {...firstPage}
-            />
-            <PlaylistVideos pages={data.pages} />
-          </>
-        )}
-      </div>
-    </div>
+    <>
+      <PlaylistInfo
+        title={firstPage.name}
+        thumbnail={firstPage.thumbnailUrl}
+        uploaderName={firstPage.name}
+        {...firstPage}
+      />
+      <PlaylistVideos pages={data.pages} />
+    </>
   )
 }
